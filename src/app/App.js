b9ea26bf3d4f7f2b5538881ff2cd436908eb07ce
Payam Yektamaram <payamyek@gmail.com>
2022-12-25 14:33:49 -0500

    refactor: about statement

--- src/app/App.js
+++ src/app/App.js
@@ -65,11 +65,12 @@ let AboutMe = () => (
     </div>
     <div className="flex flex-row">
       <p className="md:text-xl text-justify text-white text-sm">
-        I'm a<span className="font-bold">&nbsp;full-stack developer&nbsp;</span>
+        I am a<span className="font-bold">&nbsp;full-stack developer&nbsp;</span>
         currently working at the<span className="font-bold">&nbsp;Municipal Property Assessment Corporation.&nbsp;</span>
-        I have always been interested in technology and building new things
-        which is why I'm currently learning<span className="font-bold">&nbsp;Rust&nbsp;</span>in hopes
-        of better canvassing the imperative programming scene!
+        I have always been interested in technology and building new things.
+        Which is why I am currently learning<span className="font-bold">&nbsp;Rust&nbsp;</span>in hopes
+        of using it to power a new<span className="font-bold">&nbsp;chess engine&nbsp;</span>
+        I am currently building!
       </p>
     </div>
   </div>
