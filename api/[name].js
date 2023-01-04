739d079329f9cb4c44102834f342f1c75e948423
Payam Yektamaram <payamyek@gmail.com>
2023-01-03 20:44:59 -0500

    feat: simple endpoint to get all records

--- api/[name].js
+++
@@ -1,6 +0,0 @@
-export default function handler (request, response) {
-  // api/[name].js -> /api/lee
-  // req.query.name -> "lee"
-  const { name } = request.query
-  return response.json({ name })
-}
