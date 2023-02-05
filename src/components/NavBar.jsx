5bf309edb44cfc64a30a3aa54e3d9f28057496da
Payam Yektamaram <payamyek@gmail.com>
2023-02-04 20:24:21 -0500

    refactor: use .jsx for all react files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,17 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 const NavBar = (props) => {
   return (
     <div className="basis-1/4 lg:flex flex-col gap-5 pt-[5%] text-xl hidden items-center">
-      <div className="text-3xl justify-self-start  cursor-pointer">
+      <div className="text-3xl justify-self-start">
         {props.theme === 'dark' ? (
           <FontAwesomeIcon
             icon={faSun}
-            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550"
+            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550 cursor-pointer"
             onClick={() => props.setTheme('light')}
           />
         ) : (
           <FontAwesomeIcon
             icon={faMoon}
-            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550"
+            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550 cursor-pointer"
             onClick={() => props.setTheme('dark')}
           />
         )}
