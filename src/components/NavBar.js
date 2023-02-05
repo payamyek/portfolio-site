8fd27565902802fb3458e350b8849aed304f270e
Payam Yektamaram <payamyek@gmail.com>
2023-02-04 20:09:04 -0500

    fix: sidebar margins and padding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,22 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = (props) => {
   return (
-    <div className="basis-1/4 lg:flex flex-col gap-5 pt-[5%] pl-16 text-xl hidden">
-      {props.theme === 'dark' ? (
-        <FontAwesomeIcon
-          icon={faSun}
-          className="self-start text-3xl justify-self-start hover:scale-110 hover:rotate-90 transition ease-in-out delay-550 cursor-pointer"
-          onClick={() => props.setTheme('light')}
-        />
-      ) : (
-        <FontAwesomeIcon
-          icon={faMoon}
-          className="self-start text-3xl justify-self-start hover:scale-110 hover:rotate-[360deg] transition ease-in-out delay-550 cursor-pointer"
-          onClick={() => props.setTheme('dark')}
-        />
-      )}
+    <div className="basis-1/4 lg:flex flex-col gap-5 pt-[5%] text-xl hidden items-center">
+      <div className="text-3xl justify-self-start  cursor-pointer">
+        {props.theme === 'dark' ? (
+          <FontAwesomeIcon
+            icon={faSun}
+            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550"
+            onClick={() => props.setTheme('light')}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faMoon}
+            className="hover:scale-105 hover:rotate-180 transition ease-in-out delay-550"
+            onClick={() => props.setTheme('dark')}
+          />
+        )}
+      </div>
       <a
         href="/"
         className="font-bold cursor-pointer hover:dark:text-lime-200 hover:text-teal-600 pt-5">
