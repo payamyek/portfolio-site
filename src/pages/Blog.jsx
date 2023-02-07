a33c49775f7d07727e0b0b491a1d09d00f2f9086
Payam Yektamaram <payamyek@gmail.com>
2023-02-06 20:22:42 -0500

    feat: skeleton layout

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,3 +1,22 @@
-const Blog = () => <h1>Blog Posts :peepoSmile:</h1>
+const Blog = () => {
+  return (
+    <div className="flex flex-col">
+      <h1>Blog Posts</h1>
+      <BlogPost />
+    </div>
+  )
+}
+
+const BlogPost = () => {
+  return (
+    <div className="flex place-content-between py-3 text-center items-center">
+      <div className="basis-3/5 text-left">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div className="flex-1">10,000 Views</div>
+      <div className="flex-1">Posted 4 Days Ago</div>
+    </div>
+  )
+}
 
 export default Blog
