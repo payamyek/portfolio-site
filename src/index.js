8b4725c6755d7f33eaaea23ea7f43f25f9dec4da
Payam Yektamaram <payamyek@gmail.com>
2024-07-09 22:55:42 -0400

    refator: remove web vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'highlight.js/styles/default.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import { RouterProvider } from 'react-router-dom'
@@ -14,11 +13,6 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
 
 // register service worker for offline mode
 serviceWorkerRegistration.register()
